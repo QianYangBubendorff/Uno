a63d17075a5cc0fa506c1669465dba8371041573
QianYangBubendorff <qian.yangbubendorff@gmail.com>
2022-04-06 10:30:38 +0200

    uno project new size input added

--- src/at/campus02/nowa/App.java
+++ src/at/campus02/nowa/App.java
@@ -11,6 +11,7 @@ public class App {
     private boolean exit = false;
     private int figureNr;
     private Figure figure;
+    private int faktor;
 
     // Konstruktor
     // input wird verwendet um Daten vom Benutzer einzulesen (verwendet scanner)
@@ -39,25 +40,41 @@ public class App {
     private void readUserInput() {
         //TODO: Alle Eingaben der Benutzer einlesen
         inputFigure();
+        inputGroesse();
     }
 
     private void updateState() {
         //TODO: Benutzereingaben verarbeiten
-
-        switch (figureNr){
-            case 1:
-                figure = new FigureH();
-                break;
-            default: break;
+            switch (figureNr) {
+                case 1:
+                    figure = new FigureH(faktor);
+                    break;
+                case 2:
+                    figure = new FigureL(faktor);
+                    break;
+                case 3:
+                    figure = new FigureO(faktor);
+                    break;
+                case 4:
+                    figure = new FigureO2(faktor);
+                    break;
+                case 5:
+                    figure = new FigureI(faktor);
+                    break;
+                case 6:
+                    figure = new FigureMinus(faktor);
+                    break;
+                default:
+                    break;
+            }
         }
-    }
 
     private void printState() {
         //TODO: Ausgabe des aktuellen Zustands
-        if (figure != null){
-            output.println(figure);
+            if (figure != null) {
+                output.println(figure);
+            }
         }
-    }
 
     private void inputFigure() {
         // Hier sehen Sie ein Pattern für Benutzereingaben
@@ -67,6 +84,20 @@ public class App {
             figureNr = input.nextInt();
             if (figureNr < 1 || figureNr > 6) {
                 output.println("Dies ist keine gültige Grafik!");
+            } else {
+                break;
+            }
+        }while (true);
+    }
+
+    private void inputGroesse() {
+        // Hier sehen Sie ein Größe für Benutzereingaben
+        // Solange kein gültiger Wert eingegeben wurde, wird die Eingabe wiederholt
+        do{
+            output.println("Bitte wählen Sie eine Größe (1-3)");
+            faktor = input.nextInt();
+            if (faktor < 1 || faktor > 3) {
+                output.println("Dies ist keine gültige Größe!");
             } else {
                 break;
             }
--- src/at/campus02/nowa/figures/Figure.java
+++ src/at/campus02/nowa/figures/Figure.java
@@ -2,17 +2,27 @@ package at.campus02.nowa.figures;
 
 public abstract class Figure {
     protected char[][] symbol = new char[3][3];
+    protected int faktor;
+
+
+    protected Figure() {
+    }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < 3; col++) {
-                sb.append(symbol[col][row]);
+            for (int f = 0; f < faktor; f++) {
+                for (int col = 0; col < 3; col++) {
+                    for (int g = 0; g < faktor; g++) {
+                        sb.append(symbol[col][row]);
+                    }
+                }
+                sb.append('\n');
             }
-            sb.append('\n');
-        }
 
+        }
         return sb.toString();
+
     }
 }
